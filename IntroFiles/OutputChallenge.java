adefd8ebe14a2146c7d62ff6da8cd482bbdb64fd
RicardoSerranoPena <ricardo30499@gmail.com>
2020-09-21 16:42:22 +0700

    Adding Student Challenge file

---
+++ IntroFiles/OutputChallenge.java
@@ -0,0 +1,10 @@
+public class OutputChallenge {
+
+    public static void main(String[] args) {
+        //define at least three variables and assign them a proper value
+        //like name, age, self-employed, etc...
+        //Connect these variables to make a String to be printed out
+        //You should only use print statement
+        System.out.println("This should print an introduction about yourself using at least three variables");
+    }
+}
