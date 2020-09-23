4cb3d085a7aa2596e482b5396130c5eeabec45e7
RicardoSerranoPena <ricardo30499@gmail.com>
2020-09-23 17:33:58 +0700

    2nd lessons improvements

--- RelationalOperators/SelectionStatements.java
+++ RelationalOperators/SelectionStatements.java
@@ -2,10 +2,16 @@ class SelectionStatements {
 
     public static void main (String[] args) {
 
-        int age = 8;
-        if (age >= 18) {
-            System.out.println("You are over 18");
-        } 
+        boolean hasACat = true;
+
+        if (hasACat) {
+            System.out.println("You do have a cat");
+        }
+        
+        // int age = 8;
+        // if (age >= 18) {
+        //     System.out.println("You are over 18");
+        // } 
         // else if (age <= 13) {
         //     System.out.println("You qualify for a child's discount");
         // } 
---
+++ RelationalOperators/Siblings.java
@@ -0,0 +1,20 @@
+/**
+ * This is a class used to test the student on their Selection Staments knowledge
+ */
+public class Siblings {
+    
+    /**
+     * main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Ask the user how many siblings do they have
+        //Store the user's response as an int
+        //If they don't have any siblings print out "Lucky you!" or something else
+        //If the user response is 1, then ask what is their sibling's name and store it in a variable
+        //If the user has more than 1 ask for their names
+        //Example: You have 8 siblings! What are their names?
+        //Store their response as a String
+        //Print out their names back to the user and say something about their names
+    }
+}
